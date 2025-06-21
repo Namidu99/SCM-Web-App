edc5ab8e06fe93e98cc3f21a5636b8021e82436b
Namidu99 <nami.hasalanka@gmail.com>
2025-06-21 23:39:13 +0530

    Set basename for BrowserRouter to support deployment path

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/SCM-Web-App">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
